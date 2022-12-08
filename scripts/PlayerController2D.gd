b5c710911b77551ecf00e91fab0e1e28ba922401
Bella <miracleforcemanga@gmail.com>
2022-12-08 15:17:56 -0500

    added animations and bugfixes

--- scripts/PlayerController2D.gd
+++ scripts/PlayerController2D.gd
@@ -11,6 +11,7 @@ onready var slimey = $Slimey;
 
 func _ready():
 	print(velocity.y)
+	slimey.play("Idle")
 	
 #gets the input of the player and creates movement
 func update_x():
@@ -27,25 +28,34 @@ func update_x():
 
 func _physics_process(delta):
 	update_x()
-	# If touching the ground, refresh the jump counter.
+	# If touching the ground, refresh the jump counter and set current animation state to idle.
 	if is_on_floor():
 		if GlobalVariables.canDoubleJump && GlobalVariables.hasPowerup:
 			GlobalVariables.jumpCount = 2
 		else:
 			GlobalVariables.jumpCount = 1
 		jumping = false
+		slimey.play("Idle")
+
 		
 
 	# Should we jump?
 	if Input.is_action_just_pressed("jump") and GlobalVariables.jumpCount > 0:
 		GlobalVariables.jumpCount -= 1 # One less jump!
 		jumping = true
-		#jump higher if High Jump is collected
+
+		#stop any animations playing, and play jump animation when jump is pressed and jumpCount is less than 0
+		slimey.stop()
+		slimey.play("Jump")
+		
+		#jump higher if High Jump is collected, then reset it once the powerup runs out, as to not make it overpowered
 		if GlobalVariables.canHighJump and GlobalVariables.hasPowerup:
 			GlobalVariables.jump_speed = -900
 		else:
 			GlobalVariables.jump_speed = -700
 		velocity.y = GlobalVariables.jump_speed
+
+		
 
 	# To allow controlling the jump height, instantly set the velocity when the jump key is released.
 	# Some recommend quadrupling the gravity instead.
@@ -55,7 +65,3 @@ func _physics_process(delta):
 	velocity.y += gravity * delta
 	
 	velocity = move_and_slide(velocity, Vector2.UP) 
-
-	
-
-	
